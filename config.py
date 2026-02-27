a757c25261a7a47c85f9c544cb608f8c9d75cd55
suvDMT <Suvarup.Ghosh@dmt-group.com>
2026-02-27 14:21:07 +0100

    Updated config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ import sys
 from PIL import Image
 
 # Theme path for GUI (fallback when not running as a PyInstaller bundle)
-THEME_PATH_FALLBACK = r"C:\path_to_themes\tkinter_themes\Azure-ttk-theme-main\azure.tcl"
+THEME_PATH_FALLBACK = r"\path\to\azure.tcl"
 
 # Transform parameters file for coregistration
-TRANSFORM_PARAMETERS_FILE = r"D:\Filezilla\Coreg Essen\transform_parameters.txt"
+TRANSFORM_PARAMETERS_FILE = r"\path\to\transform_parameters.txt"
 
 # Model path for HSI masking
-MASK_MODEL_PATH = r"D:\essen\05_06_2025_pth27.pth"
+MASK_MODEL_PATH = r"\path\to\your_model.pth"
 
 # Allow processing of very large images
 Image.MAX_IMAGE_PIXELS = None
